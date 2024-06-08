5e5dcc9b1d6756dcd100ac5788be68afcb4bd02b
ViniciusAugusto18 <123427245+ViniciusAugusto18@users.noreply.github.com>
2024-06-08 01:19:09 -0300

    Restartt

--- Fundamento/src/server.ts
+++ Fundamento/src/server.ts
@@ -13,6 +13,7 @@ app.register(transactionsRoutes, {
 app
   .listen({
     port: env.PORT,
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
   })
   .then(() => {
     console.log('HTTP Server Running!')
